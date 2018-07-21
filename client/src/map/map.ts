b44f72ad41f23ada0b26c9859ad2e599f0a623c2
Chris Fisher <chrisfisher236@gmail.com>
2018-07-21 13:11:37 -0500

    First commit of adding map.

--- client/src/map/map.ts
+++ client/src/map/map.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 declare const H: any;
 
@@ -6,14 +6,41 @@ declare const H: any;
     selector: 'sugar-map',
     templateUrl: './map.html'
 })
-export class MapComponent {
+export class MapComponent implements AfterViewInit {
 
     public platform = new H.service.Platform({
         'app_id': 'uf15v48L6LE8m0gPzETV',
-        'app_code': 'wFGAWN-X2t5RLJYc8Ul6A'
+        'app_code': 'wFGAWN-X2t5RLJYc8Ul6Aw',
+        useCIT: true,
+        useHTTPS: true
     });
 
-    title = 'app';
+    constructor(private parentElement: HTMLElement) {
+        console.log('here');
+    }
+
+    public ngAfterViewInit() {
+        const defaultLayers = this.platform.createDefaultLayers();
+        const map = new H.Map(
+            this.parentElement,
+            defaultLayers.normal.map,
+            {
+                zoom: 10,
+                center: { lat: 52.5, lng: 13.4 }
+            });
+
+        // // Instantiate (and display) a map object:
+        // const map = new H.Map(
+        //     document.getElementById('mapContainer'),
+        //     defaultLayers.normal.map,
+        //     {
+        //         zoom: 10,
+        //         center: { lat: 52.5, lng: 13.4 }
+        //     });
+
+
+        console.log(map);
+
+
+    }
 }
-
-
