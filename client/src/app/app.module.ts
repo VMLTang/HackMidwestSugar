252d4c93684595bc9d94f3359668b80205840310
Alex Eden <alexandereden91@gmail.com>
2018-07-21 13:08:41 -0500

    Install rxjs-compat and plugin in the OktaAuthModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { SugarSharedModule } from './shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { OktaAuthModule } from '@okta/okta-angular';
 import { MapModule } from '../map/map.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -13,8 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     SugarSharedModule,
     BrowserModule,
+    MapModule,
+    OktaAuthModule.initAuth(environment.oktaConfig),
     AppRoutingModule,
-    MapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
