9478e4089b7f4b91486fc1dfcd9beeb720779eeb
Alex Eden <alexandereden91@gmail.com>
2018-07-21 12:48:20 -0500

    Add Okta configs for the environments

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+import { OktaConfig } from '@okta/okta-angular/dist/okta/models/okta.config';
+
+const oktaProdConfig: OktaConfig = {
+  issuer: 'https://dev-974784.oktapreview.com/oauth2/default',
+  redirectUri: 'https://localhost:4200/implicit/callback',
+  clientId: '0oafs2u945PbmLYAu0h7',
+};
+
 export const environment = {
-  production: true
+  production: true,
+  oktaConfig: oktaProdConfig,
 };
