6e16ce3b6d0dcc5fa66990324cef7d1c1fed9aad
Alex Eden <alexandereden91@gmail.com>
2018-07-21 21:43:40 -0500

    Implement the expo-nav

--- src/app/expo/expo-main/expo-main.component.ts
+++ src/app/expo/expo-main/expo-main.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   templateUrl: './expo-main.component.html',
   styleUrls: ['./expo-main.component.scss']
 })
 export class ExpoMainComponent implements OnInit {
+  @HostBinding() class = 'column justify-space-between';
 
   constructor() { }
 
--- src/app/expo/expo-nav/expo-nav.component.ts
+++ src/app/expo/expo-nav/expo-nav.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
-  selector: 'sugar-expo-nav',
+  selector: 'expo-nav', // tslint:disable-line
   templateUrl: './expo-nav.component.html',
   styleUrls: ['./expo-nav.component.scss']
 })
 export class ExpoNavComponent implements OnInit {
+  @HostBinding() class = 'row align-center gap-5 justify-space-between p-x-10 p-y-0';
 
   constructor() { }
 
