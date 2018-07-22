756519e5706870913631d9b7ed0a46579d8b371f
Alex Eden <alexandereden91@gmail.com>
2018-07-22 08:57:51 -0500

    Merge fixes

--- src/app/expo/scheduler/scheduler.service.ts
+++ src/app/expo/scheduler/scheduler.service.ts
@@ -1,6 +1,10 @@
+import { switchMap, map } from 'rxjs/operators';
+import { UsersService } from '@sugar/app/core/user.service';
+import { PostsService } from '@sugar/app/core/posts.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { ExpoService } from './../expo.service';
+import { from } from 'rxjs';
 
 @Injectable()
 export class SchedulerService {
@@ -9,18 +13,51 @@ export class SchedulerService {
     private readonly route: ActivatedRoute,
     private readonly router: Router,
     public expoService: ExpoService,
-
+    public userService: UsersService,
+    public postsService: PostsService
   ) {
     console.log(`I'm the SchedulerService`);
   }
 
-  confirm() {
+  async confirm() {
+    await this.postsService.grantPosting(
+      this.userService.userId! as any,
+      (new Date()).toUTCString(),
+    );
+
     this.router.navigate(['..'], {
       relativeTo: this.route,
       queryParams: {
         confirm: true
       }
     });
+
+
+    this.expoService.currentPostId.pipe(
+      switchMap(postId => {
+        return from(this.postsService.grantPosting(
+          postId,
+          this.userService.userId! as any,
+          (new Date()).toUTCString(),
+        ));
+      })
+    )
+    // )
+    // .subscribe(() => {
+    //   this.router.navigate(['..'], {
+    //     relativeTo: this.route,
+    //     queryParams: {
+    //       confirm: true
+    //     }
+    //   });
+    // }, console.error)
+
+    //   .subscribe(user =)
+    // const user = this.userService.user$.g
+    // this.postsService.grantPosting({
+    //   id:
+
+    // })
   }
 
 
