1e9aa7b78ce50374fa622d3cfeac61bff7cddec4
Alex Eden <alexandereden91@gmail.com>
2018-07-22 06:06:28 -0500

    Flesh out the types

--- src/lib/sugar.types.ts
+++ src/lib/sugar.types.ts
@@ -1,5 +1,43 @@
 import { MapPoint } from '@sugar/lib/map.types';
 
+
+export enum PostingType {
+  Offer = 'OFFER',
+  Request = 'REQUEST'
+}
+
+export enum PostingStatus {
+  Pending = 'PENDING',
+  Complete = 'COMPLETE',
+  Expired = 'EXPIRED',
+  Cancelled = 'CANCELED'
+}
+
+export interface PostingLocation {
+  lat: number;
+  long: number;
+}
+
+export interface PostingContent {
+  message: string;
+  item: string;
+  quantity: number;
+}
+
+export interface Posting {
+  id: number;
+  type: PostingType;
+  status: PostingStatus;
+  pickupLocation: PostingLocation;
+  pickupTime: string;
+  expiresAt: string;
+  closedAt: string;
+  content: PostingContent;
+  createdAt: string;
+  createdBy: User;
+  grantedBy: User;
+}
+
 export enum UserRole {
   Consumer = 'consumer',
   Producer = 'producer',
@@ -7,6 +45,11 @@ export enum UserRole {
 
 export interface User {
   id: string;
+  cellNumber: string;
+  name: string;
+  verified: number;
+  createdAt: string;
+  updatedAt: string;
 }
 
 // TODO: REMOVE OPTIONAL-NESS
