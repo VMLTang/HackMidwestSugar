d9b98a87503a7ebb1bcbda84030968b752cf4de4
Chris Fisher <chrisfisher236@gmail.com>
2018-07-21 20:49:44 -0500

    Added some forms and some styles and then made an indeterminte spinner.

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -8,6 +8,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
@@ -36,7 +37,8 @@ import { RouterModule } from '@angular/router';
     MatIconModule,
     MatToolbarModule,
     MatInputModule,
-    MatListModule
+    MatListModule,
+    MatProgressSpinnerModule
   ],
 })
 export class SugarSharedModule {}
